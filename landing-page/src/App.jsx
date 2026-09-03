ad433b979bd4ba3c3dfa50fa7b76e52c6c9f2444
Manuel Acosta <acmanuel04@gmail.com>
2026-09-03 12:45:05 -0600

    Hero y Marquee de Sponsors

--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -1,24 +1,34 @@
 import './App.css'
 import Noticias from './components/Noticias'
 import Header from './components/Header'
+import Hero from './components/Hero'
 import SobreN from './components/SobreN'
 import Ceo from './components/ceo.jsx'
 import Footer from './Footer'
+import Sponsors from './components/Sponsors'
 
 function App() {
   return (
     <>
       
       {/*HEADER*/}
-      <div id="inicio">
-        <Header/>
-      </div>
+      <Header/>
 
+      {/*HERO*/}
+      <Hero />
+
+      {/*SPONSORS*/}
+      <Sponsors/>
+
+      <SobreN />
+      
+      <Ceo />
+      
       {/*NOTICIAS*/}
       <Noticias/>
-      <SobreN />
-      <Ceo />
+
       <Footer />
+
     </>
   )
 }
